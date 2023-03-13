edd2e73d4355a142f8feb4bf63469d33bb9e85c9
Elias Cerne <eliascerne@icloud.com>
2023-03-13 22:15:23 +0100

    feat: add drawinterface to mapmover_mapping

---
+++ mapmover_mapping/include/mapmover_slam_lib/util/DrawInterface.h
@@ -0,0 +1,16 @@
+#ifndef drawinterface_h__
+#define drawinterface_h__
+
+class DrawInterface{
+public:
+  virtual void drawPoint(const Eigen::Vector2f& pointWorldFrame) = 0;
+  virtual void drawArrow(const Eigen::Vector3f& poseWorld) = 0;
+  virtual void drawCovariance(const Eigen::Vector2f& mean, const Eigen::Matrix2f& cov) = 0;
+
+  virtual void setScale(double scale) = 0;
+  virtual void setColor(double r, double g, double b, double a = 1.0) = 0;
+
+  virtual void sendAndResetData() = 0;
+};
+
+#endif
